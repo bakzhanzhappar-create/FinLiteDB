2e96a81c9f59e330f073a58719b67e69cff83cd2
bakzhanzhappar-create <bakzhanzhappar@gmail.com>
2026-03-22 21:49:30 +0500

    all float to decimal

--- core.py
+++ core.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass, field, replace
+from dataclasses import dataclass, field#, replace
 from decimal import Decimal, ROUND_HALF_UP
-from uuid import UUID
+# from uuid import UUID
 
 
 class InvalidTemplateError(Exception):
@@ -11,22 +11,8 @@ class InvalidPaymentError(Exception):
 
 
 @dataclass(frozen=True, slots=True)
-class Bank:
-    scale: Decimal = field(default=0.0)
-    description: str = field(default="")
-    target_scale: Decimal = field(default=0.1)
-
-    def post_init(self)-> None:
-        if self.scale < 0:
-            raise InvalidPaymentError("It cant be negative")
-
-    def target_success(self)-> None:
-        if self.target_scale<=self.scale:
-
-
-@dataclass(frozen=True, slots=True)
 class Payment:
-    value: float = field(default=0.0)
+    value: Decimal=Decimal('0')
     description: str = field(default="Без описания")
 
     def post_init(self) -> None:
@@ -44,28 +30,34 @@ class Percentage(Payment):
         if self.value > 100:
             raise InvalidPaymentError("Value must be less than 100")
 
-    def apply(self, value: float) -> float:
-        return value - (self.value * value) / 100
+    def apply(self, value: Decimal) -> Decimal:
+        result=value - (self.value * value) / Decimal('100')
+        return result.quantize(Decimal('1.00'), rounding=ROUND_HALF_UP)
+
 
 @dataclass(frozen=True, slots=True)
 class Fix(Payment):
 
-    def apply(self, amount: float) -> float:
+    def apply(self, amount: Decimal) -> Decimal:
         return amount - self.value
 
 
-@dataclass(frozen=True, slots=True)
-class Bank:
-    amount: float = field(default=0.0)
-
-    def post_init(self) -> None:
-        if self.amount < 0:
-            raise InvalidTemplateError("Amount must be positive")
-
-    def add_money(self, amount: Payment) -> Bank:
-
-        return replace(self, amount=amount.value + self.amount)
-
+# @dataclass(frozen=True, slots=True)
+# class Bank:
+#     amount: Decimal=Decimal('0')
+#     description: str = field(default="")
+#     target_scale: Decimal=Decimal('0')
+#
+#
+#     def post_init(self) -> None:
+#         if self.amount < 0:
+#             raise InvalidTemplateError("Amount must be positive")
+#
+#     def add_money(self, amount: Payment) -> Bank:
+#         return replace(self, amount=amount.value + self.amount)
+#
+#     def target_success(self)-> None:
+#         if self.target_scale<=self.amount:
 
 @dataclass(frozen=True, slots=True)
 class Template:
@@ -77,19 +69,18 @@ class Template:
         if not self.payments:
             raise ValueError("Payments list cannot be empty")
 
-    def apply(self, amount: float) -> float:
+    def apply(self, amount: Decimal) -> Decimal:
         for payment in self.payments:
             amount = payment.apply(amount)
-
         return amount
 
-money: float = 52_000
+money= Decimal('52000')
 t = Template(
     payments=[
-        Percentage(30),
-        Fix(10_000),
-        Fix(20_000),
-        Fix(1_000),
+        Percentage(value=Decimal('30')),
+        Fix(Decimal('10000')),
+        Fix(Decimal('20000')),
+        Fix(Decimal('1000')),
     ]
 )
 
