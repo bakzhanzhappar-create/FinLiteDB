dedfb9948d681dc2e98fec94caaa87553c123cb5
bakzhanzhappar-create <bakzhanzhappar@gmail.com>
2026-03-23 23:56:18 +0500

    minor fix

--- core.py
+++ core.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass, field#, replace
+from dataclasses import dataclass, field, replace
 from decimal import Decimal, ROUND_HALF_UP
-# from uuid import UUID
+from uuid import UUID
 
 
 class InvalidTemplateError(Exception):
@@ -15,7 +15,7 @@ class Payment:
     value: Decimal=Decimal('0')
     description: str = field(default="Без описания")
 
-    def post_init(self) -> None:
+    def __post_init__(self) -> None:
         if self.value < 0:
             raise InvalidPaymentError("Value must be positive")
 
@@ -23,7 +23,7 @@ class Payment:
 @dataclass(frozen=True, slots=True)
 class Percentage(Payment):
 
-    def post_init(self) -> None:
+    def __post_init__(self) -> None:
         if self.value < 0:
             raise InvalidPaymentError("Value must be positive")
 
@@ -42,22 +42,22 @@ class Fix(Payment):
         return amount - self.value
 
 
-# @dataclass(frozen=True, slots=True)
-# class Bank:
-#     amount: Decimal=Decimal('0')
-#     description: str = field(default="")
-#     target_scale: Decimal=Decimal('0')
-#
-#
-#     def post_init(self) -> None:
-#         if self.amount < 0:
-#             raise InvalidTemplateError("Amount must be positive")
-#
-#     def add_money(self, amount: Payment) -> Bank:
-#         return replace(self, amount=amount.value + self.amount)
-#
-#     def target_success(self)-> None:
-#         if self.target_scale<=self.amount:
+@dataclass(frozen=True, slots=True)
+class Bank:
+    amount: Decimal=Decimal('0')
+    description: str = field(default="")
+    target_scale: Decimal=Decimal('0')
+
+
+    def __post_init__(self) -> None:
+        if self.amount < 0:
+            raise InvalidTemplateError("Amount must be positive")
+
+    def add_money(self, amount: Payment) -> Bank:
+        return replace(self, amount=amount.value + self.amount)
+
+    # def target_success(self)-> None:
+    #     if self.target_scale<=self.amount:
 
 @dataclass(frozen=True, slots=True)
 class Template:
@@ -65,7 +65,7 @@ class Template:
     payments: list[Fix | Percentage] = field(default_factory=list)
     description: str = field(default="Без описания")
 
-    def post_init(self) -> None:
+    def __post_init__(self) -> None:
         if not self.payments:
             raise ValueError("Payments list cannot be empty")
 
