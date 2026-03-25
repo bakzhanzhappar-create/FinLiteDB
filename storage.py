168719badbdc9e39cb65827f585aef62b32f303e
bakzhanzhappar-create <bakzhanzhappar@gmail.com>
2026-03-25 23:52:34 +0500

    jsonwrite draft added

--- storage.py
+++ storage.py
@@ -1,8 +1,15 @@
 from dataclasses import dataclass
+import json
 
-@dataclass(frozen=true)
-class 
+
+@dataclass(frozen=True)
+class write:
+    def writeJSON(self):
+        with open(f"{name}.json", mode ='w', encoding='utf-8') as file:
+            json.dump(self, file, ensure_ascii=False, indent=4)
+
 
 
 
 # пропиши класс чтение и записи. Почитай че нибудь потому что я думаю что запись и чтение через обычный класс а не датакласс
+#из ветки draft изучи метод как они могли создать систему с возможностью на нескольких логинов
