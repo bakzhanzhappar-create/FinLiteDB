af2ad691a3ce6912346c14a0ee0a74f315f60900
bakzhanzhappar-create <bakzhanzhappar@gmail.com>
2026-06-15 23:39:40 +0500

    routine commit

--- storage.py
+++ storage.py
@@ -2,16 +2,19 @@ import json
 from dataclasses import asdict
 from decimal import Decimal
 
+
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Decimal):
             return str(obj)
         return super().default(obj)
 
+
 def save_to_json(to_user_file):
-    with open("baga.json", mode='w', encoding="utf-8") as user_file:
+    with open("user.json", mode='w', encoding="utf-8") as user_file:
         json.dump(to_user_file, user_file, ensure_ascii=False, indent=4, cls=DecimalEncoder)
         return True
+
 
 def packing_template(test):
     print("Unpacking template")
