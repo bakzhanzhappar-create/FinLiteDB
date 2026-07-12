7f6a09f159cc86a885db43f69ca226870d0b0087
bakzhanzhappar-create <bakzhanzhappar@gmail.com>
2026-07-13 04:06:56 +0500

    sql test

--- storageSQL.py
+++ storageSQL.py
@@ -5,7 +5,19 @@ db = sqlite3.connect('bagatest.db')
 cursor= db.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS bagatabletest
+CREATE TABLE IF NOT EXISTS bagatabletest(
+    id PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS payments_table(
+    payment_type TEXT NOT NULL,
+    value FLOAT NOT NULL,
+    description TEXT
+    )
+
 """)
 
 db.close()
