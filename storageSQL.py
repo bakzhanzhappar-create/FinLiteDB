63030c198b77f68eccf1ab64de954fb551fbca93
bakzhanzhappar-create <bakzhanzhappar@gmail.com>
2026-07-13 03:37:18 +0500

    routine commit

--- storageSQL.py
+++ storageSQL.py
@@ -1,1 +1,11 @@
-#здесь буду писать аналог storage используя sqlite3
+import sqlite3
+
+db = sqlite3.connect('bagatest.db')
+
+cursor= db.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS bagatabletest
+""")
+
+db.close()
